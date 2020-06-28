2c3c044011bc474a92bad19c1e34a1a3d5703f64
nova-sangeeth <novasangeeth@outlook.com>
2020-06-28 17:02:26 +0530

    added the string token url to the main urls file (Unable to find the regex for string tokens in a url)

--- url_shortner/urls.py
+++ url_shortner/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("main.urls")),
+    path("<str:token>", views.home, name="home"),
 ]
