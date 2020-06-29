d98c995c7fb0a4f5ff0c41b9c960c8895ffed75a
nova-sangeeth <novasangeeth@outlook.com>
2020-06-29 21:26:08 +0530

    added and removed the slug field and added null args to the short url

--- main/models.py
+++ main/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class short_urls(models.Model):
-    short_url = models.CharField(max_length=20)
+    short_url = models.CharField(max_length=20, null=True)
     long_url = models.URLField("URL", unique=True)
 
