7e76326a08459245f0f797775148614e86ae2d2c
nova-sangeeth <novasangeeth@outlook.com>
2020-06-30 20:23:11 +0530

    Changed the max limit to 512 for long_urls.

--- main/models.py
+++ main/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class short_urls(models.Model):
     short_url = models.CharField(max_length=20, null=True)
-    long_url = models.URLField("URL", unique=True)
+    long_url = models.URLField("URL", max_length=512, unique=True)
 
