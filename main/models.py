483278e6f4354e9650962f64749d95e821f48726
nova-sangeeth <novasangeeth@outlook.com>
2020-07-03 02:08:54 +0530

    added the __str__ method to show the short urls in the list.

--- main/models.py
+++ main/models.py
@@ -5,3 +5,6 @@ class short_urls(models.Model):
     short_url = models.CharField(max_length=20)
     long_url = models.URLField("URL", max_length=512)
 
+    def __str__(self):
+        return self.short_url
+
