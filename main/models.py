6f02f19f8d5c4da631cf248162f6970ead7df6d3
nova-sangeeth <novasangeeth@outlook.com>
2020-07-13 21:21:30 +0530

    remved the comented lines of code from the main branch

--- main/models.py
+++ main/models.py
@@ -5,7 +5,6 @@ class short_urls(models.Model):
     short_url = models.CharField(max_length=20)
     long_url = models.URLField("URL", max_length=512)
     slug = models.CharField(max_length=16, null=True)
-    # slug = models.CharField(max_length=16, null=True, unique=True)
 
     def __str__(self):
         return self.short_url
