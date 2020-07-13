4fe50e8fa65fe6c7eb68d02417d02c167f4665b8
nova-sangeeth <novasangeeth@outlook.com>
2020-07-13 21:16:05 +0530

    added changes to the models --new field "slug"

--- main/models.py
+++ main/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class short_urls(models.Model):
     short_url = models.CharField(max_length=20)
     long_url = models.URLField("URL", max_length=512)
+    slug = models.CharField(max_length=16, null=True)
+    # slug = models.CharField(max_length=16, null=True, unique=True)
 
     def __str__(self):
         return self.short_url
