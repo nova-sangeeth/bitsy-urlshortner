1f334afa0aa8b02c64d9d7f9a9ad82b9fd0ffacd
nova-sangeeth <novasangeeth@outlook.com>
2020-08-22 14:58:21 +0530

    added the issue token method from the shortner class to keep it all under one area.

--- main/url_ext.py
+++ main/url_ext.py
@@ -13,6 +13,13 @@ def extractor(url):
     if extratcted_url.domain == "amazon":
         print("ama--shp-" + "".join(random.choice(letters) for i in range(token_size)))
     return "".join(random.choice(letters) for i in range(token_size))
+        print(token_size)
+
+    def issue_token(self):
+        letters = string.ascii_letters
+        # n = "shorter"
+        # return n + "".join(random.choice(letters) for i in range(self.token_size))
+        return "".join(random.choice(letters) for i in range(self.token_size))
 
 
 extractor(url)
