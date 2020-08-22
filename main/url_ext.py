9a2ea1d08141566bd03a667d0ab2c4578db2ffe5
nova-sangeeth <novasangeeth@outlook.com>
2020-08-22 20:32:19 +0530

    fixed the issues with te tldextract and domain recog token gen

--- main/url_ext.py
+++ main/url_ext.py
@@ -9,32 +9,62 @@ import string
 class url_sep:
     token_size = 5
 
-    def __init__(self, token_size=None):
+    def __init__(self):
         self.token_size = token_size if token_size is not None else 5
 
     def extractor(self):
         extratcted_url = tldextract.extract(self)
-        token_size = 5
+
         letters = string.ascii_letters
         if extratcted_url.domain == "amazon":
-            print(
-                "ama--shp-" + "".join(random.choice(letters) for i in range(token_size))
-            )
-        # return "".join(random.choice(letters) for i in range(token_size))
+            # print(
+            #     "ama--shp-" + "".join(random.choice(letters) for i in range(token_size))
+            # )
+            return "ama-shp".join(random.choice(letters) for i in range(token_size))
 
-            return "amz--shp-" + "".join(
-                random.choice(letters) for i in range(token_size)
-            )
+        elif extratcted_url.domain == "google":
+            # print(
+            #     "ggle-sch" + "".join(random.choice(letters) for i in range(token_size))
+            # )
+            return "ggle-sch".join(random.choice(letters) for i in range(token_size))
+        # print(token_size)
+        else:
+            return "".join(random.choice(letters) for i in range(token_size))
 
-        return "".join(random.choice(letters) for i in range(token_size))
-        print(token_size)
-
-    def issue_token(self):
-        letters = string.ascii_letters
-        # n = "shorter"
-        # return n + "".join(random.choice(letters) for i in range(self.token_size))
-        return "".join(random.choice(letters) for i in range(self.token_size))
+    # def issue_token(self):
+    #     letters = string.ascii_letters
+    #     # n = "shorter"
+    #     # return n + "".join(random.choice(letters) for i in range(self.token_size))
+    #     return "".join(random.choice(letters) for i in range(self.token_size))
 
 
-# extractor(url)
+# url_sep.extractor(url)
 # https://www.amazon.in/
+
+
+# import tldextract
+# import random
+# import string
+
+
+# url = input("Enter the url")
+
+
+# def extractor(url):
+#     extratcted_url = tldextract.extract(url)
+#     token_size = 5
+#     letters = string.ascii_letters
+#     if extratcted_url.domain == "amazon":
+#         print("ama--shp-" + "".join(random.choice(letters) for i in range(token_size)))
+#     return "".join(random.choice(letters) for i in range(token_size))
+#         print(token_size)
+
+#     def issue_token(self):
+#         letters = string.ascii_letters
+#         # n = "shorter"
+#         # return n + "".join(random.choice(letters) for i in range(self.token_size))
+#         return "".join(random.choice(letters) for i in range(self.token_size))
+
+
+# # extractor(url)
+# # https://www.amazon.in/
