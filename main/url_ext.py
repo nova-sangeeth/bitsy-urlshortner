4cbdf51a5592a3129aff252cd64e0c648ca627d6
nova-sangeeth <novasangeeth@outlook.com>
2020-08-22 15:00:06 +0530

    added methods to intialize the class and declated the limits for the token generator.

--- main/url_ext.py
+++ main/url_ext.py
@@ -6,11 +6,17 @@ import string
 url = input("Enter the url")
 
 
-def extractor(url):
-    extratcted_url = tldextract.extract(url)
+class url_sep:
     token_size = 5
-    letters = string.ascii_letters
-    if extratcted_url.domain == "amazon":
+
+    def __init__(self, token_size=None):
+        self.token_size = token_size if token_size is not None else 5
+
+    def extractor(self):
+        extratcted_url = tldextract.extract(self)
+        token_size = 5
+        letters = string.ascii_letters
+        if extratcted_url.domain == "amazon":
         print("ama--shp-" + "".join(random.choice(letters) for i in range(token_size)))
     return "".join(random.choice(letters) for i in range(token_size))
         print(token_size)
