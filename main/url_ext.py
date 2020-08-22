592039d3bb5e4bab632dcfa6942d63440d41438c
nova-sangeeth <novasangeeth@outlook.com>
2020-08-22 19:33:34 +0530

    removed the return statement and added used the print statement for testing purposes.

--- main/url_ext.py
+++ main/url_ext.py
@@ -17,6 +17,10 @@ class url_sep:
         token_size = 5
         letters = string.ascii_letters
         if extratcted_url.domain == "amazon":
+            print(
+                "ama--shp-" + "".join(random.choice(letters) for i in range(token_size))
+            )
+        # return "".join(random.choice(letters) for i in range(token_size))
 
             return "amz--shp-" + "".join(
                 random.choice(letters) for i in range(token_size)
