9985dd1e9fa26c757ff836eb32c612d02b7c4f33
nova-sangeeth <novasangeeth@outlook.com>
2020-08-23 19:54:46 +0530

    added null to the short_url field

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class short_urls(models.Model):
-    short_url = models.CharField(max_length=20)
+    short_url = models.CharField(max_length=20, null=True)
     long_url = models.URLField("URL", max_length=512)
     slug = models.CharField(max_length=16, null=True)
 
