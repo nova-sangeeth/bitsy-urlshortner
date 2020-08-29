1eee02f60db674f52aa7e828c4fbd91a51e4459b
nova-sangeeth <novasangeeth@outlook.com>
2020-08-29 20:49:47 +0530

    added new model for the UserProfile and added the name, age, country and added the user via a onetoone relation

--- main/models.py
+++ main/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class short_urls(models.Model):
     short_url = models.CharField(max_length=20, null=True)
     long_url = models.URLField("URL", max_length=512)
@@ -8,4 +11,14 @@ class short_urls(models.Model):
 
     def __str__(self):
         return self.short_url
+
+
+class UserProfile(models.Model):
+    User = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64, null=True)
+    age = models.IntegerField(null=True)
+    country = models.CharField(null=True, max_length=24)
+
+    def __str__(self):
+        return str(self.user)
 
