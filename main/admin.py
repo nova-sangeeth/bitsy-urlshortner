12cac001ad9e5121924dc85bef81e6f7b5bdc89d
nova-sangeeth <novasangeeth@outlook.com>
2020-08-29 20:49:08 +0530

    added new models to the admin panel.

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import short_urls
+from .models import short_urls, UserProfile
 
 admin.site.register(short_urls)
+admin.site.register(UserProfile)
