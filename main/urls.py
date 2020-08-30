f0f363875ecbc9996cbce3d4b80b9c3f95bd7217
nova-sangeeth <novasangeeth@outlook.com>
2020-08-31 02:21:25 +0530

    added path for the edit profile

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from .views import index, new_url, profile, registration, home
+from .views import index, new_url, profile, registration, home, edit_profile
 
 urlpatterns = [
     url(r"^$", index, name="index"),
     url(r"^new_url/$", new_url, name="new_url"),
     url(r"^profile/$", profile, name="profile"),
     url(r"^registration/$", registration, name="registration"),
+    url(r"^edit_profile/$", edit_profile, name="edit_profile"),
 ]
 
