cb6aee7c2875612d33547a1065bc733ad0fbc8e2
nova-sangeeth <novasangeeth@outlook.com>
2020-08-30 17:06:49 +0530

    added new path for the registration.

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import index, new_url, profile
+from .views import index, new_url, profile, registration
 
 urlpatterns = [
     url(r"^$", index, name="index"),
     url(r"^new_url/$", new_url, name="new_url"),
     url(r"^profile/$", profile, name="profile"),
+    url(r"^registration/$", registration, name="registration"),
 ]
 
