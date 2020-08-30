aa6905a0a48cfee343da2476a816e9419dff6df3
nova-sangeeth <novasangeeth@outlook.com>
2020-08-30 16:23:49 +0530

    added new path for the profile.

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import index, new_url
+from .views import index, new_url, profile
 
 urlpatterns = [
     url(r"^$", index, name="index"),
     url(r"^new_url/$", new_url, name="new_url"),
+    url(r"^profile/$", profile, name="profile"),
 ]
 
