4bcb82266cb106e1805280935fc998f8f9ccb455
nova-sangeeth <novasangeeth@outlook.com>
2020-09-01 20:03:08 +0530

    added the model to the admin to the user_created_url

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import short_urls, UserProfile
+from .models import short_urls, UserProfile, user_created_url
 
 admin.site.register(short_urls)
 admin.site.register(UserProfile)
+admin.site.register(user_created_url)
