1cd3ce78cf2fd0ebc0dd538cfaa053915ddb1103
nova-sangeeth <novasangeeth@outlook.com>
2020-09-01 20:33:48 +0530

    linked the view to the urls path.

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import index, new_url, profile, registration, home, edit_profile
+from .views import index, new_url, profile, registration, home, edit_profile, my_url
 
 urlpatterns = [
     url(r"^$", index, name="index"),
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r"^profile/$", profile, name="profile"),
     url(r"^registration/$", registration, name="registration"),
     url(r"^edit_profile/$", edit_profile, name="edit_profile"),
+    url(r"^my_url/$", my_url, name="my_url"),
 ]
 
