d70be6fa197fb73ff7f568f0cfb37ac36adfe876
nova-sangeeth <novasangeeth@outlook.com>
2020-09-08 18:45:20 +0530

    removed the slug field from the models for the anonymous user.

--- main/models.py
+++ main/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class short_urls(models.Model):
     short_url = models.CharField(max_length=20, null=True)
     long_url = models.URLField("URL", max_length=512)
-    slug = models.CharField(max_length=16, null=True)
+    # slug = models.CharField(max_length=16, null=True)
 
     def __str__(self):
         return self.short_url
